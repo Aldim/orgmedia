85ebee9e04f7707c109b066e9276f14c3824841f
Your Name <you@example.com>
2018-11-20 22:36:40 +0300

    initial commit

---
+++ backmodule/src/main/java/org/mediasaver/mediasaver/MediaSaverApplication.java
@@ -0,0 +1,12 @@
+package org.mediasaver.mediasaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaSaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaSaverApplication.class, args);
+	}
+}
---
+++ backmodule/src/test/java/org/mediasaver/mediasaver/MediaSaverApplicationTests.java
@@ -0,0 +1,16 @@
+package org.mediasaver.mediasaver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MediaSaverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
